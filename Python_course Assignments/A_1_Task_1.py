972087f3df7c2caebac4bd416b8f22451e1f0b2f
Anurag <anuragtpatgar@gmail.com>
2026-04-20 12:54:55 +0530

    Add files via upload

---
+++ Python_course Assignments/A_1_Task_1.py
@@ -0,0 +1,7 @@
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+print("Addition: ", float(a) + float(b))
+print("Subtraction: ", float(a) - float(b))
+print("Multiplication: ", float(a) * float(b))
+print("Division: ", float(a) / float(b))
---
+++ Python_course Assignments/A_1_Task_2.py
@@ -0,0 +1,4 @@
+fn = input("Enter you first name: ")
+ln = input("Enter you last name: ")
+
+print("Hello, %s %s! Welcome to the Python program." % (fn, ln))
