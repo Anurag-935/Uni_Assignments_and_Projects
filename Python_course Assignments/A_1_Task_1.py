5aa75150856a942bfdf89639c22e8eac96e3cfd4
Anurag <anuragtpatgar@gmail.com>
2026-04-20 12:58:27 +0530

    Update A_1_Task_1.py

--- Python_course Assignments/A_1_Task_1.py
+++ Python_course Assignments/A_1_Task_1.py
@@ -1,7 +1,9 @@
-a = input("Enter the first number: ")
-b = input("Enter the second number: ")
+#A program that takes 2 numbers as inputs and performs some basic Mathematical Operations
 
-print("Addition: ", float(a) + float(b))
-print("Subtraction: ", float(a) - float(b))
-print("Multiplication: ", float(a) * float(b))
-print("Division: ", float(a) / float(b))
+a = input("Enter the first number: ") #Inputs the 1st number
+b = input("Enter the second number: ") #Inputs the 2st number
+
+print("Addition: ", float(a) + float(b)) #ADDITION
+print("Subtraction: ", float(a) - float(b)) #SUBTRACTION
+print("Multiplication: ", float(a) * float(b)) #MULTIPLICATION
+print("Division: ", float(a) / float(b)) #DIVISION
