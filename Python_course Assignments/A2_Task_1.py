a833814d4860d596f3e8b02e74e9991b0bb2e66c
Anurag <anuragtpatgar@gmail.com>
2026-04-20 13:02:36 +0530

    Update A2_Task_1.py

--- Python_course Assignments/A2_Task_1.py
+++ Python_course Assignments/A2_Task_1.py
@@ -1,5 +1,7 @@
-n = int(input("Enter a number: "))
+# A simple program to check if the given number is even or odd
+
+n = int(input("Enter a number: ")) # Takes the number as input
 if (n%2 == 0):
-    print(f"{n} is an even number")
+    print(f"{n} is an even number") # This if block checks for the number being divisible by 2 to test if its even or odd and prints if even
 else:
-    print(f"{n} is an odd number")
+    print(f"{n} is an odd number") # Else block will print if the number is odd
