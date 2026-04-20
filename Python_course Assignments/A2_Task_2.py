0445bcc25d25f10bd1aefeae2d996b46f4bfc85b
Anurag <anuragtpatgar@gmail.com>
2026-04-20 13:05:33 +0530

    Update A2_Task_2.py

--- Python_course Assignments/A2_Task_2.py
+++ Python_course Assignments/A2_Task_2.py
@@ -1,4 +1,6 @@
-a = 0
-for i in range(51):
-    a += i
-print("The sum of number from 1 to 50 is:",a)
+#A simple program to add numbers form 0 to 50 and display it
+
+sum = 0
+for i in range(51): #for loop iterates through all the diiferent values of i and the logic accumulates and adds it
+    sum += i
+print("The sum of number from 1 to 50 is:",sum) # prints the final result
